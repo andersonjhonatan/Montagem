139efd139c21787bceddba259ab3968560464ca5
Anderson Jhonatan <105141966+andersonjhonatan@users.noreply.github.com>
2024-09-14 01:58:01 -0300

    Atualizar o Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -57,7 +57,7 @@ const Footer = () => {
                 <Link href="https://www.instagram.com/montadorseu/" target="_blank">
                   <CiInstagram size={30} className={styles.iconInstagram} />
                 </Link>
-                <Link href="https://contate.me/seumontador" target="_blank">
+                <Link href="https://contate.me/montadorseu" target="_blank">
                   <MdOutlineWhatsapp size={30} className={styles.iconInstagram} />
                 </Link>
               </div>
