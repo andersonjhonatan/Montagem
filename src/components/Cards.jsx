ac5effff040685dc279b7ceaf4e334a28807d0c1
Anderson Jhonatan <105141966+andersonjhonatan@users.noreply.github.com>
2026-08-10 15:31:55 -0300

    melhora estrutura da galeria

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,22 +1,5 @@
 'use client'
-
-import { renderizarFotos } from "@/functions/Global"
+import { renderizarFotos } from '@/functions/Global'
 import styles from '../styles/Cards.module.css'
-
-const CardsGaleria = () => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-4">
-          <div className={styles.card}>
-            <div className={styles.cardbody}>
-              <figure className={styles.figureservices}>{renderizarFotos()}</figure>
-            </div>
-          </div>  
-        </div>            
-      </div>
-    </div>
-  )
-}
-
+const CardsGaleria=()=> <section className={styles.gallery} aria-label="Galeria de trabalhos realizados"><div className={styles.grid}>{renderizarFotos()}</div></section>
 export default CardsGaleria
